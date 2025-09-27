a2205afca6ce93b0aab1879fb0bdd1cead74ddbc
ricitos2001 <cesar2001ricitos@gmail.com>
2025-09-27 16:40:13 +0200

    7-g1-vista-invertida-java-21-sequenced

--- src/main/java/com/example/geonotesteaching/GeoNotes.java
+++ src/main/java/com/example/geonotesteaching/GeoNotes.java
@@ -74,7 +74,8 @@ public class GeoNotes {
                     case 3 -> filterNotes();
                     case 4 -> exportNotesToJson();
                     case 5 -> listLatestNotes();
-                    case 6 -> running = false;
+                    case 6 -> listReversedNotes();
+                    case 7 -> running = false;
                     default -> System.out.println("❌ Opción no válida. Inténtalo de nuevo.");
                 }
             } catch (NumberFormatException e) {
@@ -95,7 +96,8 @@ public class GeoNotes {
         System.out.println("3. Filtrar notas por palabra clave");
         System.out.println("4. Exportar notas a JSON (Text Blocks)");
         System.out.println("5. Listar últimas N");
-        System.out.println("6. Salir");
+        System.out.println("6. Obtener notas revertidas");
+        System.out.println("7. Salir");
         System.out.print("Elige una opción: ");
     }
 
@@ -151,6 +153,15 @@ public class GeoNotes {
          * En Kotlin harías algo similar con forEach y String templates.
          */
         timeline.getNotes().forEach((id, note) -> System.out.printf("ID: %d | Título: %s | Contenido: %s%n", id, note.title(), note.content()));
+    }
+
+    private static void listReversedNotes() {
+        System.out.println("\n--- Notas revertidas disponibles ---");
+        if (timeline.reversed().isEmpty()) {
+            System.out.println("No hay notas creadas.");
+            return;
+        }
+        timeline.reversed().forEach((id, note) -> System.out.printf("ID: %d | Título: %s | Contenido: %s%n", id, note.title(), note.content()));
     }
 
     private static void filterNotes() {
